b31a7c1a65da991d238459804b1eca16910b9164
Sergey Orlovskiy <s_orlovskiy@florist.ru>
2023-05-14 21:26:07 +0300

    init project

---
+++ src/test/java/tests/ExampleTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class ExampleTests extends TestBase{
+
+    @Test
+    void testExample1() {
+        open();
+    }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,13 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver.exe");
+        Configuration.baseUrl = "https://www.florist.ru/";
+    }
+}
