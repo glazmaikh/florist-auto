01ba8079cf3afa9c83d3403df69e1db1ef6504e5
Sergey Orlovskiy <s_orlovskiy@florist.ru>
2023-05-22 18:31:40 +0300

    added deserialize city json to map

--- src/test/java/tests/WithApiExampleTest.java
+++ src/test/java/tests/WithApiExampleTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -12,40 +11,13 @@ import models.DataItemDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-
-import static io.restassured.RestAssured.given;
 
 public class WithApiExampleTest {
 
-    //public Root root;
-    public String name;
-    public int id;
-
     @BeforeEach
     public void setUp() {
-        // в BeforeEach делать запрос "Список всех городов" и сохранять id и name
-        // отправлять запрос в /bouquet, указывать в параметре id города
-        // результат сохранять в массив их имен
-        // искать буект по имени и кликать на него
-//        root = given()
-//                .auth().basic("florist_api", "123")
-////                .formParam("iflorist", 0)
-////                .formParam("includeMeta", 0)
-//                .when()
-//                .get("https://www.test.florist.local/api/city/top")
-//                .then()
-//                .statusCode(200)
-//                .extract().as(Root.class);
 
-        //Document doc = Jsoup.parse(client.successRedirect(user).asString());
-        //            Element successText = doc.select("div:containsOwn(Your registration completed)")
-        //                    .first();
-
-        //List<UserDataModel> dataModels = response.getData();
-        //List<String> firstNames = dataModels.stream().map(UserDataModel::getFirstName).collect(Collectors.toList());
     }
 
     @Test
@@ -59,8 +31,9 @@ public class WithApiExampleTest {
 
         ObjectMapper objectMapper = new ObjectMapper();
         DataDto data = objectMapper.readValue(body.asString(), DataDto.class);
-        System.out.println(data.getData().get("724").getName());
+        System.out.println(data.getData().get("1963").getName());
 
+        //перебирать мапу стримом
         Map<String, DataItemDto> cityMap = data.getData();
         for (Map.Entry<String, DataItemDto> entry : cityMap.entrySet())
             System.out.println("Key = " + entry.getKey() +
