1649d2e4018b2c6ca83aa05d5299f13135e96f2a
Sergey Orlovskiy <s_orlovskiy@florist.ru>
2023-05-23 22:17:11 +0300

    some clean TestData

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -6,23 +6,15 @@ import java.util.Map;
 import java.util.Random;
 
 public class TestData {
-    static Random random = new Random();
-
-    // добавить вызов апи списка всех городов
-    public String getCity() {
-        String[] cityArray = {"Москва", "Санкт-Петербург", "Астрахань", "Барнаул", "Владивосток"};
-        return getRandomArrayItem(cityArray);
-    }
 
     // добавить вызов апи списка букетов
-    //, "Небесный сапфир", "Любезный Пич", "51 инстаграм", "Хризантемы осенью", "Марио"
     public String getBouquet() {
         String[] bouquetArray = {"вечер в Баварии"};
         return getRandomArrayItem(bouquetArray);
     }
 
     public String getRandomArrayItem(String[] values) {
-        return values[random.nextInt(values.length)];
+        return values[new Random().nextInt(values.length)];
     }
 
     public DataItemDto getRandomCity(Map<String, DataItemDto> cityMap) {
