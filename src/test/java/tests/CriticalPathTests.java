0aa2581bfeba2b5bc238b66e3394a1d52140a6a2
Sergey Orlovskiy <s_orlovskiy@florist.ru>
2023-06-20 13:23:51 +0300

    moved @BeforeAll to TestBase

--- src/test/java/tests/CriticalPathTests.java
+++ src/test/java/tests/CriticalPathTests.java
@@ -1,9 +1,6 @@
 package tests;
 
-import config.BaseConfig;
 import helpers.ApiClient;
-import org.aeonbits.owner.ConfigFactory;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.*;
@@ -16,20 +13,7 @@ public class CriticalPathTests extends TestBase {
     private final OrderPage orderPage = new OrderPage(apiClient);
     private final PaymentPage paymentPage = new PaymentPage();
     private final SuccessPage successPage = new SuccessPage();
-    private static String baseUrl;
-    private static String cardNumber;
-    private static String expireNumber;
-    private static String cvcNumber;
     private String yourName, yourEmail, yourPhone, name, phone, address;
-
-    @BeforeAll
-    static void setConfig() {
-        BaseConfig config = ConfigFactory.create(BaseConfig.class, System.getProperties());
-        baseUrl = config.getBaseUrl();
-        cardNumber = config.getCardNumber();
-        expireNumber = config.getExpireNumber();
-        cvcNumber = config.getCvcNumber();
-    }
 
     @BeforeEach
     void setData() {
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,17 +2,30 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.BaseConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+    protected static String baseUrl;
+    protected static String cardNumber;
+    protected static String expireNumber;
+    protected static String cvcNumber;
     @BeforeAll
-    public static void setUp() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
+    static void setUp() {
         System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver114.exe");
         Configuration.holdBrowserOpen = true;
         //Configuration.remote = "https://login:password@selenoid.autotests.cloud/wd/hub";
+
+        BaseConfig config = ConfigFactory.create(BaseConfig.class, System.getProperties());
+        baseUrl = config.getBaseUrl();
+        cardNumber = config.getCardNumber();
+        expireNumber = config.getExpireNumber();
+        cvcNumber = config.getCvcNumber();
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @AfterAll
