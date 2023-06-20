6619c13ab099a10aec5d8f06716a77ce275862b9
Sergey Orlovskiy <s_orlovskiy@florist.ru>
2023-06-20 13:08:23 +0300

    added allure

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,22 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
     @BeforeAll
-    static void setUp() {
-        //System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver114.exe");
+    public static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver114.exe");
         Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://login:password@selenoid.autotests.cloud/wd/hub";
+        //Configuration.remote = "https://login:password@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        SelenideLogger.removeListener("allure");
     }
 }
