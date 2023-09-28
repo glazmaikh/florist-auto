f331c8dc7e8739c7a0681022dc48756f7e76873d
Sergey Orlovskiy <s_orlovskiy@florist.ru>
2023-09-28 15:14:27 +0300

    added wait for phone input when register

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -28,9 +28,9 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() throws IOException {
-//        String testEnv = System.getProperty("TEST_ENV");
-//        String propertiesFilePath = "src/test/resources/" + testEnv + ".properties";
-//        new Properties().load(new FileInputStream(propertiesFilePath));
+        String testEnv = System.getProperty("TEST_ENV");
+        String propertiesFilePath = "src/test/resources/" + testEnv + ".properties";
+        new Properties().load(new FileInputStream(propertiesFilePath));
 
         BaseConfig config = ConfigFactory.create(BaseConfig.class, System.getProperties());
         baseUrl = config.getBaseUrl();
@@ -41,12 +41,12 @@ public class TestBase {
         login = config.getLogin();
         password = config.getPassword();
 
-        System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver116.exe");
-        //Configuration.baseUrl = System.getProperty("base_url", "https://www.stage.florist.local/");
-        //Configuration.browser = System.getProperty("browser", "chrome");
+        //System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver116.exe");
+        Configuration.baseUrl = System.getProperty("base_url", "https://www.stage.florist.local/");
+        Configuration.browser = System.getProperty("browser", "chrome");
         RestAssured.baseURI = baseUrl;
-        Configuration.holdBrowserOpen = true;
-        //Configuration.remote = "http://10.201.0.139:4444/wd/hub";
+        //Configuration.holdBrowserOpen = true;
+        Configuration.remote = "http://10.201.0.139:4444/wd/hub";
 
 //        SelenideLogger.addListener("allure", new AllureSelenide());
 //        DesiredCapabilities capabilities = new DesiredCapabilities();
