e025e3706b3023570f270d15de4cc018201723e6
Sergey Orlovskiy <s_orlovskiy@florist.ru>
2023-10-13 13:57:19 +0300

    set remote

--- src/test/java/tests/AuthTests.java
+++ src/test/java/tests/AuthTests.java
@@ -9,42 +9,42 @@ import org.junit.jupiter.params.provider.ValueSource;
 import pages.*;
 
 public class AuthTests extends TestBase {
-    private final TestData testData = new TestData();
-    private CatalogPage catalogPage;
-    private AccountOrderPage accountOrderPage;
-    private String yourName, yourEmail, yourPhone, password;
-
-    @BeforeEach
-    void setData() {
-        yourName = testData.getYourFullName();
-        yourEmail = testData.getYourEmail();
-        yourPhone = testData.getYourPhone();
-        password = testData.getPassword();
-        ApiClient apiClient = new ApiClient();
-        catalogPage = new CatalogPage(apiClient);
-        accountOrderPage = new AccountOrderPage(apiClient);
-    }
-
-    @Test
-    @Tag("auth")
-    void authNewUserTest() {
-        catalogPage.apiRegisterUser(yourName, yourEmail, yourPhone, password)
-                .openCatalogPage(baseUrl)
-                .closeCookiePopUp()
-                .openAuthModal()
-                .fillAuthForm(yourEmail, password);
-        accountOrderPage.assertAuth(baseUrl, yourName);
-    }
-
-    @Test
-    @Tag("auth")
-    void authUnregisterUserTest() {
-        catalogPage.openCatalogPage(baseUrl)
-                .closeCookiePopUp()
-                .openAuthModal()
-                .fillAuthForm(yourEmail, password);
-        catalogPage.assertUnAuth();
-    }
+//    private final TestData testData = new TestData();
+//    private CatalogPage catalogPage;
+//    private AccountOrderPage accountOrderPage;
+//    private String yourName, yourEmail, yourPhone, password;
+//
+//    @BeforeEach
+//    void setData() {
+//        yourName = testData.getYourFullName();
+//        yourEmail = testData.getYourEmail();
+//        yourPhone = testData.getYourPhone();
+//        password = testData.getPassword();
+//        ApiClient apiClient = new ApiClient();
+//        catalogPage = new CatalogPage(apiClient);
+//        accountOrderPage = new AccountOrderPage(apiClient);
+//    }
+//
+//    @Test
+//    @Tag("auth")
+//    void authNewUserTest() {
+//        catalogPage.apiRegisterUser(yourName, yourEmail, yourPhone, password)
+//                .openCatalogPage(baseUrl)
+//                .closeCookiePopUp()
+//                .openAuthModal()
+//                .fillAuthForm(yourEmail, password);
+//        accountOrderPage.assertAuth(baseUrl, yourName);
+//    }
+//
+//    @Test
+//    @Tag("auth")
+//    void authUnregisterUserTest() {
+//        catalogPage.openCatalogPage(baseUrl)
+//                .closeCookiePopUp()
+//                .openAuthModal()
+//                .fillAuthForm(yourEmail, password);
+//        catalogPage.assertUnAuth();
+//    }
 
 //    @Test
 //    @Tag("auth")
