7a20f593c0bdc9121093d7b03d7acc08b05d36b3
Sergey Orlovskiy <s_orlovskiy@florist.ru>
2024-02-01 18:46:07 +0300

    uncommited createNewOrderWith2BouquetsTest()

--- src/test/java/models/bouquet/BouquetDataItemDto.java
+++ src/test/java/models/bouquet/BouquetDataItemDto.java
@@ -29,8 +29,7 @@ public @Data class BouquetDataItemDto {
     public String description;
     private Map<String, PriceItemDto> prices;
     @JsonProperty("min_date_price")
-    public Map<String, Map<String, Double>> minDatePrice;
-    //public Map<String, MinDatePrice> minDatePrice;
+    public Map<String, MinDatePrice> minDatePrice;
     public String salon_name;
 
     public MinPrice getMin_price() {
